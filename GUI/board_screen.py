f0c621d107b9483627425d2a90524e2a28e7ac7f
(ENG) <167337048+Toqa-Gamal@users.noreply.github.com>
2025-12-17 10:57:06 +0200

    Refine GUI comments for wall features: GUI handles visual representation and user interaction for walls; separated from board logic

--- GUI/board_screen.py
+++ GUI/board_screen.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from Core.board import Board
 
+
 class BoardView(QWidget):
     # Signal to go back to main menu
     backToMenu = pyqtSignal()
